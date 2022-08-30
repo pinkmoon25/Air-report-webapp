b116c6956ac2df07f68fb78ad2052f7d67276e57
Tushar <singhtushar348@gmail.com>
2022-08-30 06:51:54 +0100

    Reorganise folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
-    <div>
-      <p>Clean React App</p>
-    </div>
+    <Homepage />
   );
 }
 
---
+++ src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div>
+    <ul>
+      <li>Api data</li>
+      <li>Api data</li>
+      <li>Api data</li>
+      <li>Api data</li>
+      <li>Api data</li>
+      <li>Api data</li>
+    </ul>
+  </div>
+);
+
+export default Homepage;
