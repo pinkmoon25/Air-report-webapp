b15a8fb9b4ec830aaadfd4d507041686fe9e1b65
Tushar <singhtushar348@gmail.com>
2022-08-31 18:24:48 +0100

    Create homepage, render countries filtered by continents

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchCountries } from '../redux/action-reducer';
 
-const Homepage = () => (
-  <div>
+const Homepage = () => {
+  const countries = useSelector((state) => state.countries);
+  const dispatch = useDispatch();
+  const [region, setRegion] = useState('Asia');
+
+  const handleChange = (e) => {
+    setRegion(e.target.value);
+    dispatch(fetchCountries(e.target.value));
+  };
+
+  useEffect(() => {
+    dispatch(fetchCountries(region));
+  }, []);
+
+  const renderCountries = () => (
     <ul>
-      <li>Api data</li>
-      <li>Api data</li>
-      <li>Api data</li>
-      <li>Api data</li>
-      <li>Api data</li>
-      <li>Api data</li>
+      {countries.map((country) => (
+        <li key={country.ccn3}>
+          <p>{country.name.common}</p>
+          <img src={country.flags.svg} className="flags-img" alt="country flag" />
+          <p>
+            Capital:
+            {country.capital}
+          </p>
+          <p>
+            Population:
+            {country.population}
+          </p>
+          <Link to={`/details/${country.name.common}`}>Air report</Link>
+        </li>
+      ))}
     </ul>
-  </div>
-);
+  );
+
+  return (
+    <>
+      <div>
+        <span>select a region: </span>
+        <select id="region" value={region} onChange={(e) => handleChange(e)}>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Africa">Africa</option>
+          <option value="North America">North America</option>
+          <option value="South America">South America</option>
+          <option value="Antarctic">Antarctic</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+      {renderCountries()}
+    </>
+  );
+};
 
 export default Homepage;
