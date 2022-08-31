d7252fb57dc14a6e0a3d2200d724e894b34b582c
Tushar <singhtushar348@gmail.com>
2022-08-31 22:04:49 +0100

    Add maps of slected region

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCountries } from '../redux/action-reducer';
+import asiaMap from '../images/Asia.png';
+import africMap from '../images/africa.jpg';
+import antarcticMap from '../images/antarctic.jpg';
+import europeMap from '../images/europe.png';
+import northAmericaMap from '../images/north-america.jpg';
+import oceaniaMap from '../images/oceania.jpg';
+import southAmericaMap from '../images/south_america.jpg';
 
 const Homepage = () => {
   const countries = useSelector((state) => state.countries);
@@ -11,6 +18,27 @@ const Homepage = () => {
   const handleChange = (e) => {
     setRegion(e.target.value);
     dispatch(fetchCountries(e.target.value));
+  };
+
+  const continentMap = () => {
+    switch (region) {
+      case 'Asia':
+        return (<img src={asiaMap} alt="asia map" className="map-img" />);
+      case 'Europe':
+        return (<img src={europeMap} alt="asia map" className="map-img" />);
+      case 'Africa':
+        return (<img src={africMap} alt="asia map" className="map-img" />);
+      case 'North America':
+        return (<img src={northAmericaMap} alt="asia map" className="map-img" />);
+      case 'South America':
+        return (<img src={southAmericaMap} alt="asia map" className="map-img" />);
+      case 'Antarctic':
+        return (<img src={antarcticMap} alt="asia map" className="map-img" />);
+      case 'Oceania':
+        return (<img src={oceaniaMap} alt="asia map" className="map-img" />);
+      default:
+        return 'Map loading...';
+    }
   };
 
   useEffect(() => {
@@ -38,8 +66,8 @@ const Homepage = () => {
   );
 
   return (
-    <>
-      <div>
+    <section className="homepage">
+      <div className="select-region">
         <span>select a region: </span>
         <select id="region" value={region} onChange={(e) => handleChange(e)}>
           <option value="Asia">Asia</option>
@@ -51,8 +79,12 @@ const Homepage = () => {
           <option value="Oceania">Oceania</option>
         </select>
       </div>
+      <div className="continent">
+        <h2>{region}</h2>
+        {continentMap()}
+      </div>
       {renderCountries()}
-    </>
+    </section>
   );
 };
 
