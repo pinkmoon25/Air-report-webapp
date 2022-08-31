eab47ded1b60c833aff456ec3294049dda316238
Tushar <singhtushar348@gmail.com>
2022-08-31 18:29:53 +0100

    Add BrowserRouter, Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import Details from './components/Details';
 
 function App() {
   return (
-    <Homepage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/details/:countryname" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
