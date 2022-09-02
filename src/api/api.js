569d842017f76c3cacdc337e70bec48f89ba8c5a
Tushar <singhtushar348@gmail.com>
2022-09-02 11:22:51 +0100

    Modify url with https

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const countryBaseUrl = 'https://restcountries.com/v3.1/region/';
-const airPollutionUrl = 'http://api.openweathermap.org/data/2.5/air_pollution?';
+const airPollutionUrl = 'https://api.openweathermap.org/data/2.5/air_pollution?';
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?';
 const appId = '494817b67cbfec643134218cf6ffa45e';
 const countryMap = 'https://raw.githubusercontent.com/djaiss/mapsicon/33ba28808f8d32b5bae0ffada9cadd07073852e1/all/';
