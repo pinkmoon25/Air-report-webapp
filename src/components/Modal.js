e4e857615b460f5e9d5e7bc49af1c63dabaaf9b2
Tushar <singhtushar348@gmail.com>
2022-09-04 16:47:41 +0100

    Add React Modal for current location data

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,36 +1,32 @@
-import ReactModal from "react-modal";
-import { useEffect, useState } from "react";
+import ReactModal from 'react-modal';
+import { useEffect, useState } from 'react';
 
-const Modal = ()=>{
-    const [open, setIsOpen] = useState(false);
-    const[latitude, setLatitude] = useState(null);
-    const[longitude, setLongitude] = useState(null);
+const Modal = () => {
+  const [open, setIsOpen] = useState(false);
 
-    const handleClick = ()=>{
-        setIsOpen(true)
+  const handleClick = () => {
+    setIsOpen(true);
+  };
 
-    }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+    });
+  }, []);
 
-    useEffect(()=>{
-        navigator.geolocation.getCurrentPosition(function(position) {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-        });
-        console.log(latitude, longitude);
-    }, [])
-
-    return(
+  return (
     <div>
-        <button onClick={handleClick}>
-            current location data
-        </button>
-        <ReactModal isOpen={open}>
-            <h2>Modal</h2>
-            <button onClick={()=> setIsOpen(false)}>close</button>
-        </ReactModal>
+      <button type="button" onClick={handleClick}>
+        current location data
+      </button>
+      <ReactModal isOpen={open}>
+        <h2>Modal</h2>
+        <button type="button" onClick={() => setIsOpen(false)}>close</button>
+      </ReactModal>
     </div>
-        
-    )
+
+  );
 };
 
 export default Modal;
